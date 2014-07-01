35b585120d4887d42156533c1702e74c378fc379
AthrunArthur <athrunarthur@gmail.com>
2014-07-01 15:10:31 -0700

    fix quite bug

--- src/tools/slave.cpp
+++ src/tools/slave.cpp
@@ -6,6 +6,7 @@
 #include <boost/bind.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/shared_ptr.hpp>
+#include <stdio.h>
 
 
 //! This is global thing!
@@ -13,8 +14,13 @@ class Slave{
 public:
     Slave(ffnet::NetNervureFromFile & nnff)
         : m_oNNFF(nnff)
-        , m_b_svr_connected(false){}
+        , m_b_svr_connected(false)
+        , m_b_is_stopped(false){}
 
+    void        stop(){
+        m_b_is_stopped.store( true );
+    }
+    
     void        onTimerSendHearBeat(const boost::system::error_code& /*e*/,
                                     boost::asio::deadline_timer* t)
     {
@@ -25,9 +31,11 @@ public:
             msg->tcp_port() = m_oNNFF.NervureConf()->get<uint16_t>("tcp-server.port");
             m_oNNFF.send(msg, m_p_svr);
         }
-        t->expires_at(t->expires_at() + boost::posix_time::seconds(1));
-        t->async_wait(boost::bind(&Slave::onTimerSendHearBeat, this,
+        if(!m_b_is_stopped.load()){
+            t->expires_at(t->expires_at() + boost::posix_time::seconds(1));
+            t->async_wait(boost::bind(&Slave::onTimerSendHearBeat, this,
                   boost::asio::placeholders::error, t));
+        }
     }
 
     void    onConnSucc(ffnet::TCPConnectionBase *pConn)
@@ -48,7 +56,7 @@ public:
     {
         auto it = pEP;
         std::string master_addr = m_oNNFF.NervureConf()->get<std::string>("tcp-client.target-svr-ip-addr");
-        uint16_t port = m_oNNFF.NervureConf()->get<uint16_t>("tcp-server.port");
+        uint16_t port = m_oNNFF.NervureConf()->get<uint16_t>("tcp-client.target-svr-port");
 
         if(it->address().to_string() == master_addr &&
                 it->port() == port)
@@ -70,8 +78,17 @@ protected:
     ffnet::NetNervureFromFile  & m_oNNFF;
     bool    m_b_svr_connected;
     ffnet::EndpointPtr_t m_p_svr;
+    boost::atomic<bool>    m_b_is_stopped;
 };
 
+void  press_and_stop(ffnet::NetNervureFromFile & nnff, Slave & s)
+{
+    std::cout<<"Press any key to quit..."<<std::endl;
+    getc(stdin);
+    s.stop();
+    nnff.stop();
+    std::cout<<"Stopping, please wait..."<<std::endl;
+}
 int main(int argc, char *argv[])
 {
     ffnet::Log::init(ffnet::Log::TRACE, "slave.log");
@@ -87,6 +104,9 @@ int main(int argc, char *argv[])
 
     t.async_wait(boost::bind(&Slave::onTimerSendHearBeat, &s,
             boost::asio::placeholders::error, &t));
+    boost::thread monitor_thrd(boost::bind(&press_and_stop, boost::ref(nnff), boost::ref(s)));
+    
     nnff.run();
+    monitor_thrd.join();
     return 0;
 }
